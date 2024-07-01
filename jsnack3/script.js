e2c4539e8210ec5e0ac67b3aeebb105bf04bef22
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-01 17:15:17 +0200

    [UPD] Created prompt with for  so user can insert 4 value

---
+++ jsnack3/script.js
@@ -0,0 +1,7 @@
+1 //creo un prompt che chiede un valore all' utente
+//inizializzo il prompt in modo che chieda 4 numeri all'utente
+
+for (let i = 0; i < 4; i++) {
+    let numero = prompt('inserisci un numero')
+    console.log(numero)
+}
