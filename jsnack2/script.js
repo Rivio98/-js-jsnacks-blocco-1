d3386dfc6520467b011d8853afe2e77e56961847
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-01 16:57:37 +0200

    [UPD] cicled prompt for answer the user 6 numbers and create a prompt

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -2,3 +2,11 @@
 
 let oddNumbers = [];
 
+//ciclo un prompt che chiede 6 numeri da inseire 
+
+for (let i = 0; i < 6; i++) {
+
+    //creo il prompt
+    let userNumber = prompt('inserisci un numero');
+    console.log(userNumber)
+}
