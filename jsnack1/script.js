afc320023948b4aaca232c8caa60739bffc0be1a
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-01 16:36:56 +0200

    [UPD]  cicled array and confront with name promp, added if and exercise done

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,5 +1,5 @@
 //creo l' array con dentro vari nomi degli invitati
-const autorizzati = ["Luca", "Pippo", "Luca", "Luigi", "Fabrizio", "Stefano", "Rivaldo"];
+const autorizzati = ["Luca", "Pippo", "Luigi", "Luigi", "Fabrizio", "Stefano", "Rivaldo"];
 
 
 //creo un prompt dove chiedo il nome all'utente
@@ -7,3 +7,18 @@ const domanda = prompt('Come ti chiami?');
 
 //crea una variabile di controllo per dirmi se il nome è presente o meno nell array
 let controllo = false;
+
+//ciclo l'array e confronto l'elemento che sto ciclando con la parola inserita dall'prompt
+for (let i = 0; i < autorizzati.length; i++) {
+    //se è vera la condizione imposto la variabile di controllo a true
+    if (autorizzati[i].toLowerCase() == domanda) {
+        controllo = true;
+    }
+}
+// se la variabile è true il messaggio sarà : Accesso consentito!
+if (controllo == true) {
+    console.log(`${domanda.toLowerCase()} IL suo invito è valido!`);
+}
+else { //altrimenti : Accesso negato!
+    console.log(`${domanda.toLowerCase()} Lei non è in lista!`);
+}
